ef9271d19c85a29b3739ca946019c55bd5186d85
hunshenl <hunshen.lau@gmail.com>
2018-12-09 03:43:56 +0800

    beautify homepage

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
   mount_uploaders :attachments, EventUploader
 
   validates :name, :description, :address, :postal_code, :state, :date, :start_hr, :start_min, :end_hr, :end_min, :max_pax, :price_per_pax, :user_id, presence: true
-  validates :max_pax, :price_per_pax, numericality: { greater_than_or_equal_to: 1 }
+  validates :max_pax, :price_per_pax, numericality: { greater_than_or_equal_to: 0 }
   validates :postal_code, format: { with: /\d{5}/, message: "Postal code must be a 5 digit number" }
 
   # This method returns the number of places left for a certain event
